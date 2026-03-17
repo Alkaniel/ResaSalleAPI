19f9b3e1deaff6bfb37d2a86a02b0146138830b5
alkaniel <contact@enzo-giardinneli.fr>
2026-03-17 15:58:14 +0100

    first commit

---
+++ src/main/java/fr/enzogiardinelli/rsa/ResaSalleApiApplication.java
@@ -0,0 +1,13 @@
+package fr.enzogiardinelli.rsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResaSalleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResaSalleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/enzogiardinelli/rsa/ResaSalleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.enzogiardinelli.rsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResaSalleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
