4939ed9247fd16a5c863d5b925abb3eadb61c1ec
alkaniel <contact@enzo-giardinneli.fr>
2026-04-02 22:13:35 +0200

    Add CORS configuration to allow requests from specific VPN IPs

--- src/main/java/fr/enzogiardinelli/rsa/api/security/config/SecurityConfig.java
+++ src/main/java/fr/enzogiardinelli/rsa/api/security/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import fr.enzogiardinelli.rsa.api.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -31,6 +32,7 @@ public class SecurityConfig {
         http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
